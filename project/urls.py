c4da8dc5f5abf425734e1c78d5552f28ac336041
Yousef Elmesalamy <115108581+yousefelmesalamy@users.noreply.github.com>
2023-10-29 17:03:53 +0200

    ALLOWED_HOSTS = ['*']

--- project/urls.py
+++ project/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('multivendor.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
